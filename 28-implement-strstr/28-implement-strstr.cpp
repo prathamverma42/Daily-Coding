0f1b31b553b7e7babe5494805759a29c4803ff25
prathamverma42 <56119387+prathamverma42@users.noreply.github.com>
2022-07-03 22:07:25 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (60.75%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int s = haystack.length();
-        int p = needle.length();
-        for(int i=0;i<s;i++){
-            if(haystack[i]==needle[0]){
-                int k=1;
-                int r=i+1;
-                while(k<p){
-                    if(haystack[r]==needle[k]){
-                        r++;
-                        k++;
-                    }else{
-                        break;
-                    }
-                }
-                if(k==p){
-                    return r-p;
+        
+       // same approach with good variables and method 
+        int C = 0, pos = -1;
+        for (int i = 0; i < haystack.size(); i++) {
+            if (haystack[i] == needle[0]) {
+                pos = i;
+                C++;
+                for (int j = 1; j < needle.size(); j++) {
+                    if (haystack[i + j] == needle[j]) C++;
+                    else { C = 0; pos = -1; break; }
                 }
             }
+            if (C == needle.size()) break;
         }
-        return -1;
+        return pos;
+        
+        // int s = haystack.length();
+        // int p = needle.length();
+        // for(int i=0;i<s;i++){
+        //     if(haystack[i]==needle[0]){
+        //         int k=1;
+        //         int r=i+1;
+        //         while(k<p){
+        //             if(haystack[r]==needle[k]){
+        //                 r++;
+        //                 k++;
+        //             }else{
+        //                 break;
+        //             }
+        //         }
+        //         if(k==p){
+        //             return r-p;
+        //         }
+        //     }
+        // }
+        // return -1;
     }
 };
