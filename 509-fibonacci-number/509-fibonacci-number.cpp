0798a4165810a158809887618f73d15e87d8edfd
prathamverma42 <56119387+prathamverma42@users.noreply.github.com>
2022-07-06 10:15:56 +0530

    Time: 4 ms (52.45%), Space: 5.9 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
+    //Iterative Solution
     int fib(int n) {
-        if(n==1 || n==0) return n;
-        return fib(n-1) + fib(n-2);
+        if(n==0||n==1) return n;
+        int a=0,b=1,c=0;
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
     }
+    //Recursive Solution
+    // int fib(int n) {
+    //     if(n==1 || n==0) return n;
+    //     return fib(n-1) + fib(n-2);
+    // }
 };
