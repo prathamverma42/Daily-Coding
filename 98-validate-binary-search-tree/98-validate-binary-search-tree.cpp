0c7505e7b8674b869b08d3b38d63133d79c41654
prathamverma42 <56119387+prathamverma42@users.noreply.github.com>
2022-07-10 10:03:32 +0530

    Time: 31 ms (9.68%), Space: 21.7 MB (70.02%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,19 +11,41 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>&ans){
-        if(root==NULL) return;
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
+    
+    bool isValidBSTHelper (TreeNode* currentNode, TreeNode* min, TreeNode* max) {
+        if (currentNode==NULL) return true;               
+        if (min && currentNode->val <= min->val) return false;
+        if (max && currentNode->val >= max->val) return false;
+
+        return isValidBSTHelper (currentNode->left, min, currentNode) && isValidBSTHelper (currentNode->right, currentNode, max);
     }
+    
     bool isValidBST(TreeNode* root) {
-        vector<int> ans;
-        inorder(root,ans);
-        int n= ans.size();
-        for(int i=0;i<n-1;i++){
-            if(ans[i]>=ans[i+1]) return false;
-        }
-        return true;
+        return isValidBSTHelper (root, NULL, NULL);
     }
+//     bool check(TreeNode *root,long MIN,long MAX){
+//         if(root==NULL) return true;
+//         if(root->val<=MIN || root->val>=MAX) return false;
+        
+//         return (root->left,MIN,root->val)&&(root->right,root->val,MAX);
+//     }
+//     bool isValidBST(TreeNode* root) {
+//         return check(root,INT_MIN,INT_MAX);
+//     }
+    
+    // void inorder(TreeNode* root, vector<int>&ans){
+    //     if(root==NULL) return;
+    //     inorder(root->left,ans);
+    //     ans.push_back(root->val);
+    //     inorder(root->right,ans);
+    // }
+    // bool isValidBST(TreeNode* root) {
+    //     vector<int> ans;
+    //     inorder(root,ans);
+    //     int n= ans.size();
+    //     for(int i=0;i<n-1;i++){
+    //         if(ans[i]>=ans[i+1]) return false;
+    //     }
+    //     return true;
+    // }
 };
