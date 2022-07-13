e5979e948d3705af711745063a7e3bbb411cdcd3
prathamverma42 <56119387+prathamverma42@users.noreply.github.com>
2022-07-13 21:52:06 +0530

    Time: 17 ms (10.24%), Space: 9 MB (19.05%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -21,26 +21,50 @@ public:
 
 class Solution {
 public:
-    Node* cloneGraph(Node* node) {
-        if(node==NULL){
-            return NULL;
+    
+    //using DFS traversal
+    
+    void dfs(Node* node, map<Node*,Node*> &mp){
+        if(mp.find(node)==mp.end()){
+            mp[node]=new Node(node->val,{});
         }
-        map<Node*,Node*> mp;
-        Node*temp = new Node(node->val,{});
-        mp[node]=temp;
-        queue<Node*> q;
-        q.push(node);
-        while(!q.empty()){
-            auto p = q.front();
-            q.pop();
-            for(auto x: p->neighbors){
-                if(mp.find(x)==mp.end()){
-                    mp[x]= new Node(x->val,{});
-                    q.push(x);
-                }
-                mp[p]->neighbors.push_back(mp[x]);
+        for(auto it: node->neighbors){
+            if(mp.find(it)==mp.end()){
+                dfs(it,mp);
             }
+            mp[node]->neighbors.push_back(mp[it]);
         }
+    }
+    Node* cloneGraph(Node* node){
+        if(!node) return NULL;
+        map<Node*,Node*> mp;
+        dfs(node,mp);
         return mp[node];
     }
+    
+    
+    //using BFS traversal
+    
+    // Node* cloneGraph(Node* node) {
+    //     if(node==NULL){
+    //         return NULL;
+    //     }
+    //     map<Node*,Node*> mp;
+    //     Node*temp = new Node(node->val,{});
+    //     mp[node]=temp;
+    //     queue<Node*> q;
+    //     q.push(node);
+    //     while(!q.empty()){
+    //         auto p = q.front();
+    //         q.pop();
+    //         for(auto x: p->neighbors){
+    //             if(mp.find(x)==mp.end()){
+    //                 mp[x]= new Node(x->val,{});
+    //                 q.push(x);
+    //             }
+    //             mp[p]->neighbors.push_back(mp[x]);
+    //         }
+    //     }
+    //     return mp[node];
+    // }
 };
