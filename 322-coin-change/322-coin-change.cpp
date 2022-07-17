e7055d9bce4eab3d7919c15b8078e5a3dca3f182
prathamverma42 <56119387+prathamverma42@users.noreply.github.com>
2022-07-17 17:35:20 +0530

    Time: 76 ms (89.55%), Space: 13.2 MB (73.24%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
+    int dp[10001];
+    int func(vector<int> &coins, int amount){
+        if(amount==0) return 0;
+        if(dp[amount]!=-1) return dp[amount];
+        int ans = INT_MAX;
+        for(auto coin: coins){
+            if(amount>=coin)
+                ans = min(ans+0LL,func(coins,amount-coin)+1LL);
+        }
+        return dp[amount]=ans;
+    }
     int coinChange(vector<int>& coins, int amount) {
-        int MAX = 1e5;
-        int n = coins.size();
-        int t[n+1][amount+1];
-        for(int i=0;i<=n;i++){
-            t[i][0]=0;
-        }
-        for(int i=0;i<=amount;i++){
-            t[0][i]=MAX-1;
-        }
-        for(int i=1;i<=amount;i++){
-            if(i%coins[0]==0){
-                t[1][i]=i/coins[0];
-            }else{
-                t[1][i]=MAX-1;
-            }
-        }
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<=amount;j++){
-                if(coins[i-1]<=j){
-                    t[i][j]=min(1+t[i][j-coins[i-1]],t[i-1][j]);
-                }else{
-                    t[i][j]=t[i-1][j];
-                }
-            }
-        }
-        
-        // for(int i=0;i<=n;i++){
-        //     for(int j=0;j<=amount;j++){
-        //         cout<<t[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        return t[n][amount]==99999?-1:t[n][amount];
+        memset(dp,-1,sizeof(dp));
+        int ans = func(coins,amount);
+        return ans==INT_MAX?-1:ans;
     }
 };
