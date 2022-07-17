eaf7d84ab91bd132ebb75d00402e3ec0adc2ec8a
prathamverma42 <56119387+prathamverma42@users.noreply.github.com>
2022-07-17 21:19:56 +0530

    Time: 2297 ms (5.01%), Space: 175.1 MB (5.08%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-   int findMaxForm(vector<string>& strs, int m, int n){
-        vector<vector<int>> dp(m+1 , vector<int>(n+1));
-        
-        for(auto &s:strs){
-            int x = count(s.begin(),s.end(),'1');
-            int y = s.size()-x;
-            
-            for(int i=m; i>=y; i--)
-                for(int j=n; j>=x; j--)
-                    dp[i][j] = max(dp[i][j], 1+dp[i-y][j-x]);
+    int dp[101][101][601];
+   vector<int> cal(string s){
+        int one = 0;
+        int zero= 0;
+        int i = 0;
+        while(s[i]!='\0'){
+            if(s[i]=='0'){zero++;}
+            else one++;
+            i++;
         }
-       
-        return dp[m][n];
+        return {zero,one};
+    }
+    int solve(vector<string> &str, int m, int n, int indx){
+        if(indx==str.size()){return 0;}
+        if(m<=0 && n<=0){return 0;}
+        if(dp[m][n][indx]!=-1) return dp[m][n][indx];
+        vector<int> len=cal(str[indx]);
+        if(len[0]<=m && len[1]<=n){
+            return dp[m][n][indx]=max(1+solve(str,m-len[0],n-len[1],indx+1),solve(str,m,n,indx+1));
+        }
+        return dp[m][n][indx]=solve(str,m,n,indx+1);
+    }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(strs,m,n,0);
     }
 };
